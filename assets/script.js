1a400267d78e46f8e13aebfceb20c3554d2cdc4f
benjammo <118003873+benjammo@users.noreply.github.com>
2023-01-09 21:15:09 -0500

    added JS flow logic comments, intro to fetch, API keys added but not functional, basic wireframe for results.html, added reset.css (normalize with license

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,47 @@
 console.log("hello, world!")
 
+
+// grab user input from forms 
+
 let inputStartPoint = document.getElementById("inputStartPoint");
 let inputDestination = document.getElementById("inputDestination");
 
-//fetch weather API
 
 //fetch maps API
+fetch('http://example.com/movies.json')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
-//function to pull maps data with input
+
+//fetch geocode API
+
+fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + //location from user input
+'&limit=5&appid={API key}')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+// grab lat and lon from json above
+
+
+//fetch weather API
+
+fetch('https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=' + //starting point lat
+ + '&lon=' + //starting point lon 
+ + &appid=0e40d92140fd2b7333031cf611ffeb1e')
+.then((response) => response.json())
+.then((data) => console.log(data));
+
+
+
+fetch('https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=' + //destination lat
+ + '&lon=' + //destination lon 
+ + &appid=0e40d92140fd2b7333031cf611ffeb1e')
+.then((response) => response.json())
+.then((data) => console.log(data));
 
 //function to display map
 
 localStorage.setItem("startPoint", inputStartPoint);
 localStorage.setItem("destination", inputDestination);
+
+//change localstorage to query parameters and use localstorage for history
